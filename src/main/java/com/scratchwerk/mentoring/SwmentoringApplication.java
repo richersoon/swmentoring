5a2680f1f4488396655cd92acd4cfd2b20aa5b01
Ronnie King <Ronnie King>
2016-07-10 18:25:07 -0400

    Latest

--- src/main/java/com/scratchwerk/mentoring/SwmentoringApplication.java
+++ src/main/java/com/scratchwerk/mentoring/SwmentoringApplication.java
@@ -1,11 +1,15 @@
 package com.scratchwerk.mentoring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwmentoringApplication {
 
+	@Autowired
+	private UserRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SwmentoringApplication.class, args);
 	}
